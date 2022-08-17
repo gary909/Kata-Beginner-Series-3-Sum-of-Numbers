23e080f9938f96fa9c34c493d4a085533386f5d5
gary909 <garywhite61@hotmail.com>
2022-08-17 12:46:51 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function getSum(a, b) {
+    //Good luck!
+}
+
+console.log(getSum(0, -1)); // -1
+console.log(getSum(0, 1)); // 1
+console.log(getSum(1, 1)); // 1
+console.log(getSum(1, 2)); // 3
+console.log(getSum(-1, 2)); // 2
