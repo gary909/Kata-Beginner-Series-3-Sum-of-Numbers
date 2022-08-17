d6cab3aeae332742d467c3e4e696034103bdc191
gary909 <garywhite61@hotmail.com>
2022-08-17 13:14:54 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 function getSum(a, b) {
     //Good luck!
+    let myNum = 0;
+    let minNum = Math.min(a, b);
+    let maxNum = Math.max(a, b);
+    // if (a == b) {
+    //     return a;
+    // } else if (a < b) {
+    //     for (let i = 0; i < myLength; i++) {
+    //         myNum = myNum + i
+    //     }
+    // } else {
+    //     for (let i = 0; i < a + 1; i++) {
+    //         myNum = myNum + i
+    //     }
+    // }
+    // return myNum;
+
+    // return minNum;
+
+    return (maxNum - minNum + 1) * (minNum + maxNum) / 2;
 }
 
 console.log(getSum(0, -1)); // -1
